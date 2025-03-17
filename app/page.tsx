ecdb620ced7f5f37f032466bcd0c20f1995f5384
Aditya <adityabasak7031@gmail.com>
2025-03-17 14:39:46 +0530

    add feature component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
+import Feature from "@/components/Feature"
 import Hero from "@/components/Hero"
+
 
 const Home = () => {
   return (
     <div className="min-h-screen w-full">
 
       <Hero />
+      <Feature />
     </div>
   )
 }
