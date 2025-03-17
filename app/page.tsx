cbaec446ca36747469db180160d95530d3907448
Aditya <adityabasak7031@gmail.com>
2025-03-17 14:13:05 +0530

    rm nav

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
-
 import Hero from "@/components/Hero"
-import Navbar from "@/components/Navbar"
 
 const Home = () => {
   return (
     <div className="min-h-screen w-full">
-      <Navbar />
+
       <Hero />
     </div>
   )
