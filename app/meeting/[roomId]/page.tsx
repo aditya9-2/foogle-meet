b7e49c54bf887371452ee97b92a83ca63a2fa276
Aditya <adityabasak7031@gmail.com>
2025-03-18 16:39:20 +0530

    create meeting room

--- app/meeting/[roomId]/page.tsx
+++ app/meeting/[roomId]/page.tsx
@@ -1,3 +1,6 @@
+import ChatPanel from '@/components/ChatPannel';
+import MessageSquareElement from '@/components/MessageSquare';
+import { VideoContainer } from '@/components/VideoContainer';
 import React from 'react'
 
 const MeetingRoom = async ({ params }: {
@@ -8,7 +11,20 @@ const MeetingRoom = async ({ params }: {
     const roomId = (await params).roomId;
 
     return (
-        <div className='w-full min-h-screen py-20 text-black dark:text-white'>{roomId}</div>
+        <div className="flex h-auto py-20 overflow-hidden">
+            {/* Main content area with videos */}
+            <div className="flex-1 p-4 ">
+                <VideoContainer />
+            </div>
+
+            {/* Chat sidebar */}
+            <div className="w-[350px] border-l border-border h-full flex flex-col bg-background">
+                <MessageSquareElement />
+                <div className="flex-1 ">
+                    <ChatPanel roomId={roomId} />
+                </div>
+            </div>
+        </div>
     )
 }
 
